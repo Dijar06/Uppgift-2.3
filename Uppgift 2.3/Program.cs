fcc905b4b59aff1cb14e272bc00fe8fa6f8c5022
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-15 12:46:30 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra en lamborghini?");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer tänker du köra?");
+            float y = float.Parse(Console.ReadLine());
+            float z = 300 + 1 * y + 500 * x - 500;
+            Console.WriteLine("Priset blir totalt " + z + " kronor");
+        }
+    }
+}
